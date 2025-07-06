7f9b1b6f53d1370b33c5c060a94f937a0f3afd51
Max <max@Noutbuk-Apple.local>
2025-07-06 17:21:21 +0300

    Solution2

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,11 +1,82 @@
 'use strict';
 
+const { fillTank } = require("./fillTank");
+
 describe('fillTank', () => {
   // const { fillTank } = require('./fillTank');
 
   it('should ', () => {
 
   });
+  
+  it(`should fill tank correctly and update money`, () => {
+  const customer = {
+    money: 3000,
+    vehicle: {
+      maxTankCapacity: 40,
+      fuelRemains: 8,
+    }
+  };
 
-  // write tests here
+  fillTank(customer, 40, 30);
+
+  expect(customer.vehicle.fuelRemains).toBe(38);
+  expect(customer.money).toBe(1800);
+});
+  it(`should fill tank correctly and update money`, () => {
+  const customer = {
+    money: 3000,
+    vehicle: {
+      maxTankCapacity: 40,
+      fuelRemains: 8,
+    }
+  };
+
+  fillTank(customer, 40);
+
+  expect(customer.vehicle.fuelRemains).toBe(40);
+  expect(customer.money).toBe(1720);
+});
+  it(`should fill tank correctly and update money`, () => {
+  const customer = {
+    money: 3000,
+    vehicle: {
+      maxTankCapacity: 40,
+      fuelRemains: 8,
+    }
+  };
+
+  fillTank(customer, 40, 40);
+
+  expect(customer.vehicle.fuelRemains).toBe(40);
+  expect(customer.money).toBe(1720);
+});
+  it(`should fill tank correctly and update money`, () => {
+  const customer = {
+    money: 1000,
+    vehicle: {
+      maxTankCapacity: 40,
+      fuelRemains: 8,
+    }
+  };
+
+  fillTank(customer, 40, 30);
+
+  expect(customer.vehicle.fuelRemains).toBe(33);
+  expect(customer.money).toBe(0);
+});
+  it(`should fill tank correctly and update money`, () => {
+  const customer = {
+    money: 3000,
+    vehicle: {
+      maxTankCapacity: 40,
+      fuelRemains: 8,
+    }
+  };
+
+  fillTank(customer, 40, 1);
+
+  expect(customer.vehicle.fuelRemains).toBe(8);
+  expect(customer.money).toBe(3000);
+});
 });
